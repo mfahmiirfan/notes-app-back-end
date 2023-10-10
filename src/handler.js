a9d049939bb9866efd4d8b11e379aa0349dd5642
Fahmi <mfahmiirfan@gmail.com>
2023-10-11 01:19:55 +0700

    show all notes

--- src/handler.js
+++ src/handler.js
@@ -34,4 +34,11 @@ const addNoteHandler = (request, h) => {
       return response;
   };
 
-module.exports = { addNoteHandler };
+  const getAllNotesHandler = () => ({
+    status: 'success',
+    data: {
+      notes,
+    },
+  });
+
+  module.exports = { addNoteHandler, getAllNotesHandler };
